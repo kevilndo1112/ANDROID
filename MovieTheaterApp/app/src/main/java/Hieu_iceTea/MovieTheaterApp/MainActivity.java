e21b6f846775e49923f6ef37cb7138ced66ee0fc
KevilnDo <53203770+kevilndo1112@users.noreply.github.com>
2021-08-14 03:16:23 +0700

    ..

--- MovieTheaterApp/app/src/main/java/Hieu_iceTea/MovieTheaterApp/MainActivity.java
+++ MovieTheaterApp/app/src/main/java/Hieu_iceTea/MovieTheaterApp/MainActivity.java
@@ -1,7 +1,9 @@
 package Hieu_iceTea.MovieTheaterApp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,31 +14,43 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.widget.Toolbar;
 
 import Hieu_iceTea.MovieTheaterApp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.vung_ky_uc_chillies);
+
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        setSupportActionBar(binding.toolbar);
+        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());*/
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -67,10 +81,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
+    public void btnPlaySound_onClick(View view) {
+
+        Button btnPlay = findViewById(R.id.btnPlay);
+
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            btnPlay.setText("Pause");
+        } else {
+            mediaPlayer.pause();
+            btnPlay.setText("Play");
+        }
+
+    }
+
+    /*@Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 }
