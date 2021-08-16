b37417c65385ecc56a05f124ac3e57e6bffb622d
KevilnDo <53203770+kevilndo1112@users.noreply.github.com>
2021-08-16 17:07:12 +0700

    ...

---
+++ TicTacToe/app/src/main/java/Hieu_iceTea/TicTacToe/MainActivity.java
@@ -0,0 +1,14 @@
+package Hieu_iceTea.TicTacToe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
